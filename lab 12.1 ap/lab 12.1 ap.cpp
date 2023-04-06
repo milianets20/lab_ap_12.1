ebfb23322c785d7806aef2d12716e4a59596f644
milianets20 <115105067+milianets20@users.noreply.github.com>
2023-04-06 23:43:11 +0300

    Add files via upload

---
+++ lab 12.1 ap/lab 12.1 ap.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Elem
+{
+	Elem* link1,
+		* link2;
+	int info;
+};
+
+int main()
+{
+	Elem* p;
+
+	p = new Elem; // 1 
+	p->info = 1; // 2 
+	p->link1 = new Elem;//3
+	p->link1->info = 4;  //4 
+	p->link1->link2 = NULL;  //5 
+	p->link1->link1 = new Elem;  //6 
+	p->link1->link1->info = 3;  //7 
+	p->link1->link1->link2 = NULL;  //8 
+	p->link1->link1->link1 = new Elem;  //9 
+	p->link1->link1->link1->info = 2;  //10 
+	p->link1->link1->link1->link2 = p->link1->link1;  //11 
+	p->link1->link1->link1->link1 = p->link1;  //12 
+	p->link1->link2 = p->link1->link1->link1;  //13 
+
+
+	delete p->link1->link1->link1; // 14
+	delete p->link1->link1; // 15
+	delete p->link1; // 16
+	delete p; // 17
+	return 0;
+}
